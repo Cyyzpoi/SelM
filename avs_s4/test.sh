49b556569c10a07ce0f9db833dd118fe8a5dcfa3
Cyyzpoi <chunchunpoi233@gmail.com>
2024-07-31 18:19:56 +0800

    update

--- avs_s4/test.sh
+++ avs_s4/test.sh
@@ -4,6 +4,6 @@ visual_backbone="resnet" # "resnet" or "pvt"
 python test.py \
     --session_name ${setting}_${visual_backbone} \
     --visual_backbone ${visual_backbone} \
-    --weights "../S4_R50.pth" \
+    --weights "../pretrained model/S4_R50.pth" \
     --test_batch_size 2 \
     --save_pred_mask  \
